17bcd95fdb63d07e61f13cfed7a543d647e317dc
Kevetic <Kevetic@gmail.com>
2023-12-07 21:57:54 -0600

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-deploy",
+  base: "/r3f-practice/",
   plugins: [react()],
 });
